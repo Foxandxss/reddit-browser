0ae33d4cf8a03894067f86f4efcc4913a4860c69
Jesus Rodriguez <Foxandxss@gmail.com>
2017-12-23 20:05:56 +0100

    created new tabs and deleted old

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { ImagesPage } from '../images/images';
+import { AlbumsPage } from '../albums/albums';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  imagesTab = ImagesPage;
+  albumsTab = AlbumsPage;
 
   constructor() {
 
